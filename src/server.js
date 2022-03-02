3637579a8c6c6c108338adb6df8f1a1cf3787ed4
Marius <marius@example.com>
2022-03-02 15:04:50 +0200

    server ready

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const PORT = process.env.SERVER_PORT || 3000;
+
+const app = express();
+
+// middleware
+app.use(morgan('common'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello express');
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
