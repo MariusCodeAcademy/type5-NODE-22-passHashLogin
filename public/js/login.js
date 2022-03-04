8345b02c16fde29e28b3b8ed55d8901bda21b4e2
Marius <marius@example.com>
2022-03-04 14:03:14 +0200

    post login works

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 console.log('login');
-
+const BASE_URL = 'http://localhost:3000';
 const formEl = document.forms.login;
 
 // sustabdyti formos nustatytahi siuntima ir perkrovima
@@ -20,4 +20,15 @@ formEl.addEventListener('submit', (event) => {
 // 3. siunciam su fetch prisijungimo duomenis i back end /login
 async function loginUser(loginUserData) {
   console.log('we are tryin to login with ===', loginUserData);
+  // siunciam post request ir pridedam formoje ivestu duomenis
+  const resp = await fetch(`${BASE_URL}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(loginUserData),
+  });
+  // pasiverciam gauta atsakyma i js objekta ar masyva ar stringa
+  const respInJs = await resp.json();
+  console.log('respInJs ===', respInJs);
 }
