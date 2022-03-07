fa40fb42596838efb04ef0353289a344a8e736c2
Marius <marius@example.com>
2022-03-07 16:01:24 +0200

    function getCategories() fe

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,3 +1,13 @@
+const URL = 'http://localhost:3000';
 // parsisiusti visas kategorijas su fetch
-
 // sugeneruoti jas formoj nurodant value ir name
+async function getCategories() {
+  const resp = await fetch(`${URL}/categories`);
+  const dataInJs = await resp.json();
+  console.log('dataInJs ===', dataInJs);
+  // if (dataInJs.success === true) {
+  //   renderPosts(dataInJs.data, postContainerEl);
+  // }
+}
+
+getCategories();
