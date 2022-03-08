3e8ebffdd5514facec4eafb0480ff16a42f1293a
Marius <marius@example.com>
2022-03-08 13:33:11 +0200

    postsRoutes.post('/', async (req, res) => {

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllPostsWCategories } = require('../model/postModel');
+const { getAllPostsWCategories, insertNewPost } = require('../model/postModel');
 
 const postsRoutes = express.Router();
 
@@ -15,6 +15,21 @@ postsRoutes.get('/', async (req, res) => {
     success: true,
     data: posts,
   });
+});
+
+postsRoutes.post('/', async (req, res) => {
+  const { title, body, category_id } = req.body;
+  const insertResult = await insertNewPost(title, body, category_id);
+  if (insertResult === false) {
+    res.status(500);
+    return;
+  }
+  if (insertResult.affectedRows !== 1) {
+    res.status(400).json('no post created');
+    return;
+  }
+  // console.log('insertResult ===', insertResult);
+  res.json('Post created');
 });
 
 // GET /posts/cat/:cat_id
